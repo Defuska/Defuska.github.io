4536790bb04b54066ec4036b1ac210bf493b1899
Ninjabel <pawel200326598@wp.pl>
2025-06-09 21:01:34 +0200

    fix github pages deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://Defuska.github.io",
-  base: "/defuska",
 
   integrations: [],
 
